48594324f9f9b1ac0c0c65b2311966074cea0004
Joelito-FDC <joelito.fdc@gmail.com>
2024-04-03 10:54:49 +0800

    added login

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,23 +1,41 @@
 <?php
 
+App::uses('Security', 'Utility');
+
 class UsersController extends AppController {   
     public $helpers = array('Html', 'Form', 'Flash');
     public $components = array('Flash');
-
+    
     public function login() {
-        $this->layout = '';
-    }
-
-    public function registration() {
         $this->layout = '';
 
         if($this->request->is('post')) {
+            $email = $this->request->data['User']['email'];
+            $pass = Security::hash($this->request->data['User']['password'], 'sha1', true);
+
+            $auth = $this->User->find('first', array('conditions' => array('User.email' => $email, 'User.password' => $pass)));
+
+            if(!$auth) {
+                $this->Flash->set('Authentication unsuccessful.', array('clear' => true));
+            } else {
+                $this->Flash->set('Authentication success.', array('clear' => true));
+            }            
+        }
+    }
+    
+    public function registration() {        
+        $this->layout = '';
+        
+        if($this->request->is('post')) {
             $this->User->create();
+
+            $pass = $this->request->data['User']['password'];
+            $this->request->data['User']['password'] = Security::hash($pass, 'sha1', true);
 
             if($this->User->save($this->request->data)) {
                 return $this->redirect(array('controller' => 'users', 'action' => 'registered'));
             } else {
-                $this->Flash->error('Unable to register.');
+                $this->Flash->error('Unable to register.', array('clear' => true));
             }
         }
     }
